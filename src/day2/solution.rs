6a32a17d8b447317fe209baa270cade9dbe71ea1
Lesnek <milan.lesnek@kiwi.com>
2021-12-04 18:39:11 +0100

    Solution for day 2, part 2

--- src/day2/solution.rs
+++ src/day2/solution.rs
@@ -6,7 +6,7 @@ pub fn solution1(data: Vec<(String, i32)>) -> (i32, i32) {
             "forward" => horizontal += value,
             "down" => depth += value,
             "up" => depth -= value,
-            _ => panic!("Je to v pici")
+            _ => panic!("Je to v piči")
         }
     }
     (horizontal, depth)
@@ -24,5 +24,38 @@ fn test_solution_1() {
             ("forward".to_string(), 2),
         ]),
         (15, 10)
+    )
+}
+
+pub fn solution2(data: Vec<(String, i32)>) -> (i32, i32) {
+    let mut horizontal = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    for (direction, value) in data {
+        match direction.as_str() {
+            "forward" => {
+                horizontal += value;
+                depth += aim * value;
+            },
+            "down" => aim += value,
+            "up" => aim -= value,
+            _ => panic!("Je to v piči")
+        }
+    }
+    (horizontal, depth)
+}
+
+#[test]
+fn test_solution_2() {
+    assert_eq!(
+        solution2(vec![
+            ("forward".to_string(), 5),
+            ("down".to_string(), 5),
+            ("forward".to_string(), 8),
+            ("up".to_string(), 3),
+            ("down".to_string(), 8),
+            ("forward".to_string(), 2),
+        ]),
+        (15, 60)
     )
 }
