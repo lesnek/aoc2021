8d03563c991819026036bc2bbef196799e56ca15
Lesnek <milan.lesnek@kiwi.com>
2021-12-04 20:36:13 +0100

    Solution for day 3, both parts

---
+++ src/day3/solution.rs
@@ -0,0 +1,117 @@
+pub fn solution1(data: Vec<String>) -> (u32, u32) {
+    let mut gamma_list: Vec<u32> = vec![0; data[0].len()];
+    let mut gamma = "".to_string();
+    let mut epsilon = "".to_string();
+
+    let half_of_total = (data.len() as u32) / 2;
+    for line in data {
+        for (index, value) in line.chars().into_iter().enumerate() {
+            if value.to_string() == "1" {
+                gamma_list[index] += 1
+            }
+        }
+    }
+    for value in gamma_list {
+        if value < half_of_total {
+            gamma.push('0');
+            epsilon.push('1');
+        } else {
+            gamma.push('1');
+            epsilon.push('0');
+        }
+    }
+    let bin_gamma = u32::from_str_radix(gamma.as_str(), 2).unwrap();
+    let bin_epsilon = u32::from_str_radix(epsilon.as_str(), 2).unwrap();
+    (bin_gamma, bin_epsilon)
+}
+
+#[test]
+fn test_solution_1() {
+    assert_eq!(
+        solution1(vec![
+            "00100".to_string(),
+            "11110".to_string(),
+            "10110".to_string(),
+            "10111".to_string(),
+            "10101".to_string(),
+            "01111".to_string(),
+            "00111".to_string(),
+            "11100".to_string(),
+            "10000".to_string(),
+            "11001".to_string(),
+            "00010".to_string(),
+            "01010".to_string(),
+        ]),
+        (22, 9)
+    )
+}
+
+pub fn solution2(data: Vec<String>) -> (u32, u32) {
+    let bin_oxygen =
+        u32::from_str_radix(filter_by_chem_type(data.clone(), "oxygen")[0].as_str(), 2).unwrap();
+
+    let bin_co2 = u32::from_str_radix(filter_by_chem_type(data, "co2")[0].as_str(), 2).unwrap();
+    (bin_oxygen, bin_co2)
+}
+
+fn filter_by_chem_type(mut data: Vec<String>, chem_type: &str) -> Vec<String> {
+    let mut position = 0;
+    while data.len() > 1 {
+        let (data0, data1) = filter_data(data, position);
+        data = choose_data(data0, data1, chem_type);
+        position += 1;
+    }
+    data
+}
+
+fn choose_data(data0: Vec<String>, data1: Vec<String>, chem_type: &str) -> Vec<String> {
+    match chem_type {
+        "oxygen" => {
+            if data1.len() >= data0.len() {
+                return data1;
+            }
+            data0
+        }
+        "co2" => {
+            if data0.len() <= data1.len() {
+                return data0;
+            }
+            data1
+        }
+        _ => panic!("Je to v pici, to se neda dychat ty vole!"),
+    }
+}
+
+fn filter_data(data: Vec<String>, position: u32) -> (Vec<String>, Vec<String>) {
+    let mut data1 = Vec::new();
+    let mut data0 = Vec::new();
+    for line in data {
+        if line.chars().nth(position as usize).unwrap().to_string() == "1" {
+            data1.push(line);
+        } else {
+            data0.push(line);
+        }
+    }
+    (data0, data1)
+}
+
+#[test]
+fn test_solution_2() {
+    assert_eq!(
+        solution2(vec![
+            "00100".to_string(),
+            "11110".to_string(),
+            "10110".to_string(),
+            "10111".to_string(),
+            "10101".to_string(),
+            "01111".to_string(),
+            "00111".to_string(),
+            "11100".to_string(),
+            "10000".to_string(),
+            "11001".to_string(),
+            "00010".to_string(),
+            "01010".to_string(),
+        ]),
+        (23, 10)
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 pub mod day1;
 pub mod day2;
+pub mod day3;
 pub mod read;
 
 fn main() {
     // println!("DAY 1");
-    // let data1 = read::file_to_vec("data/day1.txt");
+    // let data1 = read::file_to_u32_vec("data/day1.txt");
     // println!("Total increased is: {}", day1::solution::solution1(data1.clone()));
     // println!("Total increased sums is: {}", day1::solution::solution2(vec![1, 2, 3, 4]));
     //
@@ -21,15 +22,32 @@ fn main() {
     //     "Horizontal is {} and depth is {} and their multiply is {}",
     //     result2_2.0, result2_2.1, result2_2.0 * result2_2.1
     // );
+    let data3 = read::file_to_str_vec("data/day3.txt");
+    let result3_1 = day3::solution::solution1(data3.clone());
+    println!(
+        "Gamma is {} and epsilon is {} and their multiply is {}",
+        result3_1.0,
+        result3_1.1,
+        result3_1.0 * result3_1.1
+    );
+    let result3_2 = day3::solution::solution2(data3);
+    println!(
+        "Oxygen is {} and co2 is {} and their multiply is {}",
+        result3_2.0,
+        result3_2.1,
+        result3_2.0 * result3_2.1
+    );
 }
-
 
 #[test]
 fn final_tests() {
-    let data1 = read::file_to_vec("data/day1.txt");
+    let data1 = read::file_to_u32_vec("data/day1.txt");
     assert_eq!(day1::solution::solution1(data1.clone()), 1298);
     assert_eq!(day1::solution::solution2(data1.clone()), 1248);
     let data2 = read::file_to_tuple("data/day2.txt");
     assert_eq!(day2::solution::solution1(data2.clone()), (1944, 1049));
     assert_eq!(day2::solution::solution2(data2.clone()), (1944, 954969));
+    let data3 = read::file_to_str_vec("data/day3.txt");
+    assert_eq!(day3::solution::solution1(data3.clone()), (3437, 658));
+    assert_eq!(day3::solution::solution2(data3), (3995, 1696));
 }
